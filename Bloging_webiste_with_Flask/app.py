ab72a0bea2a833fccef79f994342979d611c695b
Sajjad Haider <sajjadhaider7000@gmail.com>
2025-01-07 14:19:01 +0500

    7jan2025
    solve the html css loading problem by creating separate static directory, provided proper bootstrap link, also create and view flask database, next put data through register page dynamically

--- Bloging_webiste_with_Flask/app.py
+++ Bloging_webiste_with_Flask/app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///mydb.db' 
+db =SQLAlchemy(app)
+
+
+class User(db.Model): 
+    id= db.Column (db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False) 
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    fname = db.Column(db.String(120), unique=True, nullable=False)
+    lname = db.Column(db.String(120), unique=True, nullable=False)
+    pswd = db.Column(db.String(120), unique=True, nullable=False)
+    
+    def _repr_(self): 
+        return '<User %r>' % self.username
 
 @app.route("/main")
 def main():
